5fce2cfb28cba92eb03cb5ce4474205dff41137e
stefangrosaru <dgrosaru@outlook.com>
2021-11-18 10:22:27 +0000

    initial release

---
+++ Formula/quadriga.rb
@@ -0,0 +1,12 @@
+class Getfilesize < Formula
+    desc "Create Secure Tunnels to your web application"
+    homepage ""
+    url ""
+    sha256 ""
+    license "NONE"
+    version "0.0.1"
+  
+    def install
+      bin.install "quadriga"
+    end
+  end
