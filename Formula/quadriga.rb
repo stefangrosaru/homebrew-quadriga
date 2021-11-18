3d37ecf3bcfb6394b81e382bcf4df56a11656fdc
stefangrosaru <dgrosaru@outlook.com>
2021-11-18 10:30:22 +0000

    initial release

--- Formula/quadriga.rb
+++ Formula/quadriga.rb
@@ -1,4 +1,4 @@
-class Getfilesize < Formula
+class Quadriga < Formula
     desc "Create Secure Tunnels to your web application"
     homepage "https://github.com/stefangrosaru/homebrew-quadriga"
     url "https://github.com/stefangrosaru/homebrew-quadriga/releases/download/0.0.1/quadriga.tar.gz"
