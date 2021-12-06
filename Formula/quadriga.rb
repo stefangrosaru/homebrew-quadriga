e190cc86073092d8bf8f2b0e2b308cb7960f8b3b
stefangrosaru <dgrosaru@outlook.com>
2021-12-06 21:49:49 +0000

    add version 0.0.6

--- Formula/quadriga.rb
+++ Formula/quadriga.rb
@@ -1,10 +1,10 @@
 class Quadriga < Formula
-    desc "Create Secure Tunnels to your web application"
+    desc "Create Secure Tunnels for your web application"
     homepage "https://github.com/stefangrosaru/homebrew-quadriga"
-    url "https://github.com/stefangrosaru/homebrew-quadriga/releases/download/0.0.5/quadriga.tar.gz"
-    sha256 "cad1e28037d35551bdd05cd8a976a9eb62ca9e59d9eefc5e995eea33048559c3"
+    url "https://github.com/stefangrosaru/homebrew-quadriga/releases/download/0.0.6/quadriga.tar.gz"
+    sha256 "038aba553702352d621c003f23d4b7aa8ea43318a264de5c2a73653b8a7491d7"
     license "NONE"
-    version "0.0.5"
+    version "0.0.6"
   
     def install
       bin.install "quadriga"
