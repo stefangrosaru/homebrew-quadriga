dbc2a70968f9f29c26b5604cb5f206ee968c51cc
stefangrosaru <dgrosaru@outlook.com>
2022-01-01 22:21:40 +0000

    update sha sum

--- Formula/quadriga.rb
+++ Formula/quadriga.rb
@@ -2,7 +2,7 @@ class Quadriga < Formula
     desc "Create Secure Tunnels for your web application"
     homepage "https://github.com/stefangrosaru/homebrew-quadriga"
     url "https://github.com/stefangrosaru/homebrew-quadriga/releases/download/1.0.1/quadriga.tar.gz"
-    sha256 "b694db78dc3832fa1726a7b3e48e543d2810af0a37aa6663595d2dbef768e3df"
+    sha256 "439d362a6eab2f797b9de97161c94d691bc762ded4f6076dd4eeaa49b818f0e7"
     license "NONE"
     version "1.0.1"
   
